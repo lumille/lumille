f1a6f0db0cf0139b5f3d95c00464abe46e69df7f
Frederic KOLLER <koller.f@gmail.com>
2019-08-21 10:25:11 +0200

    Add Scanner path

--- src/Utility/Scan.php
+++ src/Utility/Scan.php
@@ -4,7 +4,41 @@
 namespace Lumille\Utility;
 
 
+use Lumille\Exception\ScanException;
+
 class Scan
 {
 
+    private static $codes = [
+        'dir' => 1,
+        'file' => 2
+    ];
+
+    public static function dir ($path,  $onlyExtensions = null)
+    {
+
+        if (!\file_exists($path)) {
+            throw new ScanException("Path not found", self::$codes['dir']);
+        }
+
+        $files = array_diff(\scandir($path), ['.', '..']);
+
+        if ( $onlyExtensions) {
+
+            if (!\is_array($onlyExtensions)) {
+                $onlyExtensions = [$onlyExtensions];
+            }
+
+            $_files = $files;
+            $files = [];
+            foreach  ($_files as $file) {
+                $ext = \pathinfo($file, \PATHINFO_EXTENSION);
+                if (in_array($ext, $onlyExtensions)) {
+                    $files[] = $file;
+                }
+            }
+        }
+
+        return $files;
+    }
 }
